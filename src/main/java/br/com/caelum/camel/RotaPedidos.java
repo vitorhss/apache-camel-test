f9aee8809c9991f685bcda05414506ebd821e68d
Vitor Souza <vitor.souza@b2wdigital.com>
2017-06-27 08:25:29 -0300

    Primeira rota. Utilizando componente file

--- src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -1,13 +1,26 @@
 package br.com.caelum.camel;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
 public class RotaPedidos {
 
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-		CamelContext context = new DefaultCamelContext();
+        CamelContext context = new DefaultCamelContext();
+        context.addRoutes(new RouteBuilder() {
 
-	}	
+            @Override
+            public void configure() throws Exception {
+                from("file:pedidos?delay=5s&noop=true"). 
+                log("${id} - ${body}").
+                to("file:saida");
+            }
+        });
+
+        context.start(); 
+        Thread.sleep(2000); 
+        context.start();
+    }    
 }
